aae484fce1d933f647c81e5e323eff8664950edd
mlee3142 <mil@mit.edu>
2018-01-06 08:08:49 -0500

    html audio to web audio api

--- public/mkturk_SoundPlayer.js
+++ public/mkturk_SoundPlayer.js
@@ -1,49 +1,114 @@
 class SoundPlayerClass{ 
   constructor(){
-    // todo: feed in explicit filepaths here and load; 
-    // write playSound(filename)
-    
-      this.num_redundant_buffers = 8; // because sounds do not get played if requested to be played before they are over from a previous call 
 
       this.sound_filepaths = {
       'reward_sound':'sounds/chime.wav', // chime
       'punish_sound':'sounds/bad_doot.wav', // punish sound
       'blip':'sounds/frog.wav'}
 
-      this.sound_objects = {}
       this.current_sound_counter = 0
 
       this.is_built = false
+
+
+      // https://developer.mozilla.org/en-US/docs/Web/API/BaseAudioContext/createBuffer
+
+
 }
 
   async build(){
-    for (var k in this.sound_filepaths){
-      if(this.sound_filepaths.hasOwnProperty(k)){
-        this.sound_objects[k] = []
-        for (var j = 0; j<this.num_redundant_buffers; j++){
-          var audio = new Audio(this.sound_filepaths[k])
-          audio.volume=0.15; //set boost pedal to 5% volume          
-          this.sound_objects[k].push(audio)
-        }
-      }
-    }
-    this.is_built = true
-    return 
-  }
-
-  async play_sound(name){
-    if(this.is_built == false){
-      await this.build()
-    }
-    if (!(name in this.sound_filepaths)){
+   
+    this.audioContext = new (window.AudioContext || window.webkitAudioContext)();
+    var _this = this
+    console.log('build soundplayer')
+    var finishedLoading = function(bufferList){
+      _this.bufferedSounds = bufferList 
       return 
     }
 
-    this.sound_objects[name][this.current_sound_counter].play() 
-    this.current_sound_counter = (this.current_sound_counter+1) % this.num_redundant_buffers
+    var soundRequests = [] 
+
+    var bufferLoader = new BufferLoader(this.audioContext, ['sounds/chime.wav', 'sounds/bad_doot.wav'], finishedLoading)
+    bufferLoader.load();
+    this.is_built = true
+
     return 
+
+  }
+
+  async play_sound(name){
+
+    if(this.is_built == false){
+      await this.build()
+    }
+
+    if (name == 'reward_sound'){
+      var bufferEntry = this.bufferedSounds[0]
+    }
+    else if(name == 'punish_sound'){
+      var bufferEntry = this.bufferedSounds[1]
+    }
+    else{
+      return
+    }
+    var s = this.audioContext.createBufferSource()
+    s.buffer = bufferEntry 
+    s.connect(this.audioContext.destination)
+    s.start(0)
+    //
+    return 
+
+    
   }
 }
 
+
+// Source: https://www.html5rocks.com/en/tutorials/webaudio/intro/js/buffer-loader.js
+function BufferLoader(context, urlList, callback) {
+  this.context = context;
+  this.urlList = urlList;
+  this.onload = callback;
+  this.bufferList = new Array();
+  this.loadCount = 0;
+}
+
+BufferLoader.prototype.loadBuffer = function(url, index) {
+  // Load buffer asynchronously
+  var request = new XMLHttpRequest();
+  request.open("GET", url, true);
+  request.responseType = "arraybuffer";
+
+  var loader = this;
+
+  request.onload = function() {
+    // Asynchronously decode the audio file data in request.response
+    loader.context.decodeAudioData(
+      request.response,
+      function(buffer) {
+        if (!buffer) {
+          alert('error decoding file data: ' + url);
+          return;
+        }
+        loader.bufferList[index] = buffer;
+        if (++loader.loadCount == loader.urlList.length)
+          loader.onload(loader.bufferList);
+      },
+      function(error) {
+        console.error('decodeAudioData error', error);
+      }
+    );
+  }
+
+  request.onerror = function() {
+    alert('BufferLoader: XHR error');
+  }
+
+  request.send();
+}
+
+BufferLoader.prototype.load = function() {
+  for (var i = 0; i < this.urlList.length; ++i)
+  this.loadBuffer(this.urlList[i], i);
+}
 
 
