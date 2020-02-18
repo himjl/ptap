cece7e81708d16cb78045ac2239aba086dd04ead
mlee3142 <mil@mit.edu>
2020-02-18 10:42:30 -0500

    cleanup

--- public/mkturk_TaskStreamer.js
+++ public/mkturk_TaskStreamer.js
@@ -1,25 +1,25 @@
-class TaskStreamerClass{
-    constructor(gamePackage, IB, CheckPointer){
-        this.game = gamePackage['GAME']
-        this.imageBags = gamePackage['IMAGEBAGS']
-        this.taskSequence = gamePackage['TASK_SEQUENCE']
+class TaskStreamerClass {
+    constructor(gamePackage, IB, CheckPointer) {
+        this.game = gamePackage['GAME'];
+        this.imageBags = gamePackage['IMAGEBAGS'];
+        this.taskSequence = gamePackage['TASK_SEQUENCE'];
 
-        this.TG = new TrialGeneratorClass(IB, gamePackage['IMAGEBAGS'], gamePackage['TASK_SEQUENCE'], this.game['onFinish'])
+        this.TG = new TrialGeneratorClass(IB, gamePackage['IMAGEBAGS'], gamePackage['TASK_SEQUENCE'], this.game['onFinish']);
 
-        this.CheckPointer = CheckPointer
-        
+        this.CheckPointer = CheckPointer;
+
         // State info
-        this.taskNumber = CheckPointer.get_task_number()  
-        this.trialNumberTask = CheckPointer.get_trial_number_task() 
-        this.trialNumberSession = 0
-        this.taskReturnHistory = CheckPointer.get_task_return_history()  
-        this.taskActionHistory = CheckPointer.get_task_action_history() 
-        this.taskBagHistory = CheckPointer.get_task_bag_history() 
+        this.taskNumber = CheckPointer.get_task_number();
+        this.trialNumberTask = CheckPointer.get_trial_number_task();
+        this.trialNumberSession = 0;
+        this.taskReturnHistory = CheckPointer.get_task_return_history();
+        this.taskActionHistory = CheckPointer.get_task_action_history();
+        this.taskBagHistory = CheckPointer.get_task_bag_history();
 
-        this.TERMINAL_STATE = false
-        this.monitoring = true
-        
-        
+        this.TERMINAL_STATE = false;
+        this.monitoring = true;
+
+
         this.onLoadState = {
             'taskNumber': this.taskNumber,
             'trialNumberTask': this.trialNumberTask,
@@ -29,42 +29,41 @@ class TaskStreamerClass{
             'taskBagHistory': this.taskBagHistory,
             'TERMINAL_STATE': this.TERMINAL_STATE,
             'monitoring': this.monitoring,
-        }
+        };
 
-        this.onLoadState = JSON.parse(JSON.stringify(this.onLoadState))
+        this.onLoadState = JSON.parse(JSON.stringify(this.onLoadState));
 
         this.bagSamplingWeights = undefined
     }
-    
-    async build(taskNumber, num_trials_per_stage_to_prebuffer){
+
+    async build(taskNumber, num_trials_per_stage_to_prebuffer) {
         await this.TG.build(taskNumber, num_trials_per_stage_to_prebuffer)
     }
 
-    async get_trial(){
-        if (this.taskSequence[this.taskNumber]['taskType'] == 'SR'){
-            console.log('Executing with sampling weights')
+    async get_trial() {
+        if (this.taskSequence[this.taskNumber]['taskType'] === 'SR') {
+            console.log('Executing with sampling weights');
             var tP = await this.TG.get_trial(this.taskNumber, this.bagSamplingWeights)
-        }
-        else{
+        } else {
             var tP = await this.TG.get_trial(this.taskNumber)
         }
-        
 
-        return tP 
+
+        return tP
     }
 
-    update_state(current_trial_outcome){
+    update_state(current_trial_outcome) {
 
-        var tk = this.taskSequence[this.taskNumber]
-        var r = current_trial_outcome['return']
-        var action = current_trial_outcome['action']
+        var tk = this.taskSequence[this.taskNumber];
+        var r = current_trial_outcome['return'];
+        var action = current_trial_outcome['action'];
 
-        this.taskReturnHistory.push(r)
-        this.taskActionHistory.push(action)
-        this.taskBagHistory.push(current_trial_outcome['i_sampleBag'])
+        this.taskReturnHistory.push(r);
+        this.taskActionHistory.push(action);
+        this.taskBagHistory.push(current_trial_outcome['i_sampleBag']);
 
-        this.trialNumberTask++
-        this.trialNumberSession++
+        this.trialNumberTask++;
+        this.trialNumberSession++;
 
         // ************Correction Loop ***************************
 
@@ -72,133 +71,131 @@ class TaskStreamerClass{
         // Null hypothesis: Pr(action correct | category i) is the same for all i 
         // The null is violated when, for example, the subject always chooses one option regardless of the category presented
 
-        var viewingWindowWidth = tk['correctionLoopViewingWindowLength'] || 20
-        var performanceModulationFactor = tk['correctionLoopPerformanceModulationFactor'] || 0.5 
-        if (performanceModulationFactor > 1){
+        var viewingWindowWidth = tk['correctionLoopViewingWindowLength'] || 20;
+        var performanceModulationFactor = tk['correctionLoopPerformanceModulationFactor'] || 0.5;
+        if (performanceModulationFactor > 1) {
             performanceModulationFactor = 1
         }
-        if (performanceModulationFactor < 0){
-            performanceModulationFactor = 0 
+        if (performanceModulationFactor < 0) {
+            performanceModulationFactor = 0
         }
 
-        
-        var freturn = get_sampling_weights(tk['sampleBagNames'], this.TG.idx2bag, viewingWindowWidth, this.taskReturnHistory, this.taskBagHistory, performanceModulationFactor)
 
-        this.bagSamplingWeights = freturn['samplingWeights']
-        this.performancePerBag = freturn['performancePerBag']
-        this.tStatistic = freturn['tStatistic']
-        this.empiricalEffectSize = freturn['empiricalEffectSize']
-        this.a = freturn['a']
-        this.b = freturn['b']
-        this.c = freturn['c']
-        this.d = freturn['d']
-        this.tStatistic_criticalUb = freturn['tStatistic_criticalUb']
-        this.tStatistic_criticalLb = freturn['tStatistic_criticalLb']
+        var freturn = get_sampling_weights(tk['sampleBagNames'], this.TG.idx2bag, viewingWindowWidth, this.taskReturnHistory, this.taskBagHistory, performanceModulationFactor);
 
-        if (tk['repeatSampleBagIfWrong'] != undefined){
-            if (tk['repeatSampleBagIfWrong'] == true){
+        this.bagSamplingWeights = freturn['samplingWeights'];
+        this.performancePerBag = freturn['performancePerBag'];
+        this.tStatistic = freturn['tStatistic'];
+        this.empiricalEffectSize = freturn['empiricalEffectSize'];
+        this.a = freturn['a'];
+        this.b = freturn['b'];
+        this.c = freturn['c'];
+        this.d = freturn['d'];
+        this.tStatistic_criticalUb = freturn['tStatistic_criticalUb'];
+        this.tStatistic_criticalLb = freturn['tStatistic_criticalLb'];
+
+        if (tk['repeatSampleBagIfWrong'] != undefined) {
+            if (tk['repeatSampleBagIfWrong'] == true) {
                 // Indexes tk['sampleBagNames']
-                if (r < 1){
-                    var lastBag = current_trial_outcome['sampleBag']
-                    var lastBagIdx = tk['sampleBagNames'].indexOf(lastBag)
-                    this.bagSamplingWeights = np.xvec(tk['sampleBagNames'].length, 0) // probability = 1 of repeating last bag     
+                if (r < 1) {
+                    var lastBag = current_trial_outcome['sampleBag'];
+                    var lastBagIdx = tk['sampleBagNames'].indexOf(lastBag);
+                    this.bagSamplingWeights = np.xvec(tk['sampleBagNames'].length, 0); // probability = 1 of repeating last bag
                     this.bagSamplingWeights[lastBagIdx] = 1
                 }
             }
         }
 
-        console.log(this.bagSamplingWeights)   
-        console.log('t-statistic = ', freturn['tStatistic'], '. ', this.performancePerBag,'abcd = ', this.a, this.b, this.c, this.d)
+        console.log(this.bagSamplingWeights);
+        console.log('t-statistic = ', freturn['tStatistic'], '. ', this.performancePerBag, 'abcd = ', this.a, this.b, this.c, this.d);
 
         // ***************************************
 
         // Update checkpoint 
-        var sampleBag = this.TG.get_bag_from_idx(current_trial_outcome['i_sampleBag'])
+        var sampleBag = this.TG.get_bag_from_idx(current_trial_outcome['i_sampleBag']);
         var checkpointPackage = {
-            'taskNumber': this.taskNumber, 
-            'trialNumberTask': this.trialNumberTask, 
-            'return':r, 
-            'action':action,
-            'sampleBag':sampleBag,
-            'i_sampleBag':current_trial_outcome['i_sampleBag'],
-            'i_sampleId':current_trial_outcome['i_sampleId']
-        }
-        this.CheckPointer.update(checkpointPackage)
-        this.CheckPointer.request_checkpoint_save()
+            'taskNumber': this.taskNumber,
+            'trialNumberTask': this.trialNumberTask,
+            'return': r,
+            'action': action,
+            'sampleBag': sampleBag,
+            'i_sampleBag': current_trial_outcome['i_sampleBag'],
+            'i_sampleId': current_trial_outcome['i_sampleId']
+        };
+        this.CheckPointer.update(checkpointPackage);
+        this.CheckPointer.request_checkpoint_save();
 
         // If monitoring, check transition criterion.
-        if (this.monitoring == false){
+        if (this.monitoring == false) {
             return
         }
 
-        var averageReturnCriterion = tk['averageReturnCriterion']
-        var minTrialsCriterion = tk['minTrialsCriterion']
+        var averageReturnCriterion = tk['averageReturnCriterion'];
+        var minTrialsCriterion = tk['minTrialsCriterion'];
 
-        if(averageReturnCriterion > 1){
+        if (averageReturnCriterion > 1) {
             // Assume percent if user specified above 1
-            averageReturnCriterion = averageReturnCriterion / 100 
+            averageReturnCriterion = averageReturnCriterion / 100
         }
 
-        var transition = false
-        if (this.taskReturnHistory.length >= minTrialsCriterion){
-            var averageReturn = np.mean(this.taskReturnHistory.slice(-1 * minTrialsCriterion))
-            if(averageReturn >= averageReturnCriterion){
+        var transition = false;
+        if (this.taskReturnHistory.length >= minTrialsCriterion) {
+            var averageReturn = np.mean(this.taskReturnHistory.slice(-1 * minTrialsCriterion));
+            if (averageReturn >= averageReturnCriterion) {
                 transition = true
             }
         }
 
         // Perform transition
-        if(transition == true){
-            var nextTaskNumber = this.taskNumber + 1 
-            var nextTaskReturnHistory = []
-            var nextTaskActionHistory = []
-            var nextTrialNumberTask = 0 
+        if (transition == true) {
+            var nextTaskNumber = this.taskNumber + 1;
+            var nextTaskReturnHistory = [];
+            var nextTaskActionHistory = [];
+            var nextTrialNumberTask = 0;
 
             // Check termination condition
-            if(this.taskNumber >= this.taskSequence.length-1){
-                var onFinish = this.game['onFinish']
-                if(onFinish == 'loop'){
-                    console.log('Reached end of TASK_SEQUENCE: looping')
+            if (this.taskNumber >= this.taskSequence.length - 1) {
+                var onFinish = this.game['onFinish'];
+                if (onFinish == 'loop') {
+                    console.log('Reached end of TASK_SEQUENCE: looping');
                     nextTaskNumber = 0
-                }
-                else if(onFinish == 'terminate'){
-                    console.log('Reached end of TASK_SEQUENCE: terminating')
-                    this.TERMINAL_STATE = true 
-                }
-                else if(onFinish == 'continue'){
-                    console.log('Reached end of TASK_SEQUENCE: continuing')
-                    this.monitoring = false
-                    nextTaskNumber = this.taskNumber
-                    nextTaskReturnHistory = this.taskReturnHistory 
-                    nextTaskActionHistory = this.taskActionHistory 
+                } else if (onFinish == 'terminate') {
+                    console.log('Reached end of TASK_SEQUENCE: terminating');
+                    this.TERMINAL_STATE = true
+                } else if (onFinish == 'continue') {
+                    console.log('Reached end of TASK_SEQUENCE: continuing');
+                    this.monitoring = false;
+                    nextTaskNumber = this.taskNumber;
+                    nextTaskReturnHistory = this.taskReturnHistory;
+                    nextTaskActionHistory = this.taskActionHistory;
                     nextTrialNumberTask = this.trialNumberTask
                 }
             }
 
             // Execute transition 
-            this.taskNumber = nextTaskNumber
-            this.taskReturnHistory = nextTaskReturnHistory
-            this.taskActionHistory = nextTaskActionHistory
+            this.taskNumber = nextTaskNumber;
+            this.taskReturnHistory = nextTaskReturnHistory;
+            this.taskActionHistory = nextTaskActionHistory;
             this.trialNumberTask = nextTrialNumberTask
         }
 
-        return 
+
     }
 
-    async start_buffering_continuous(){
+    async start_buffering_continuous() {
         this.TG.start_buffering_continuous()
     }
 
-    debug2record(){
+    debug2record() {
 
-        for (var k in this.onLoadState){
-            if(!this.onLoadState.hasOwnProperty(k)){
+        for (var k in this.onLoadState) {
+            if (!this.onLoadState.hasOwnProperty(k)) {
                 continue
             }
             this[k] = this.onLoadState[k]
         }
-        
-        console.log('debug2record: TaskStreamer reverted to state on load')
+
+        console.log('debug2record: TaskStreamer reverted to state on load');
         this.CheckPointer.debug2record()
     }
 }
--- public/setup_MechanicalTurk_session.js
+++ public/setup_MechanicalTurk_session.js
@@ -1,40 +1,40 @@
 async function setup_mechanicalturk_session(sessionPackage) {
 
-    GAME_PACKAGE = sessionPackage['GAME_PACKAGE']
-    GAME = GAME_PACKAGE['GAME']
-    IMAGEBAGS = GAME_PACKAGE['IMAGEBAGS']
-    TASK_SEQUENCE = GAME_PACKAGE['TASK_SEQUENCE']
+    GAME_PACKAGE = sessionPackage['GAME_PACKAGE'];
+    GAME = GAME_PACKAGE['GAME'];
+    IMAGEBAGS = GAME_PACKAGE['IMAGEBAGS'];
+    TASK_SEQUENCE = GAME_PACKAGE['TASK_SEQUENCE'];
 
-    ENVIRONMENT = sessionPackage['ENVIRONMENT']
+    ENVIRONMENT = sessionPackage['ENVIRONMENT'];
 
-    var landingPageURL = sessionPackage['LANDING_PAGE_URL']
-    console.log('Detected landing page URL', landingPageURL)
+    var landingPageURL = sessionPackage['LANDING_PAGE_URL'];
+    console.log('Detected landing page URL', landingPageURL);
 
-    SESSION = {}
-    SESSION['workerId'] = az.get_workerId_from_url(landingPageURL)
-    SESSION['hitId'] = az.get_hitId_from_url(landingPageURL)
-    SESSION['assignmentId'] = az.get_assignmentId_from_url(landingPageURL)
-    SESSION['inSandboxMode'] = az.detect_sandbox_mode(landingPageURL)
+    SESSION = {};
+    SESSION['workerId'] = az.get_workerId_from_url(landingPageURL);
+    SESSION['hitId'] = az.get_hitId_from_url(landingPageURL);
+    SESSION['assignmentId'] = az.get_assignmentId_from_url(landingPageURL);
+    SESSION['inSandboxMode'] = az.detect_sandbox_mode(landingPageURL);
     //SESSION['ipAddress'] = await az.get_ip_address()
-    SESSION['species'] = 'human_turker'
-    SESSION['url'] = window.location.href
-    SESSION['landingPageURL'] = landingPageURL
-    SESSION['agentId'] = SESSION['workerId']
-    SESSION['unixTimestampPageLoad'] = window.performance.timing.navigationStart
+    SESSION['species'] = 'human_turker';
+    SESSION['url'] = window.location.href;
+    SESSION['landingPageURL'] = landingPageURL;
+    SESSION['agentId'] = SESSION['workerId'];
+    SESSION['unixTimestampPageLoad'] = window.performance.timing.navigationStart;
 
-    console.log('SESSION', SESSION)
-    SIO = new S3_IO()
-    IB = new ImageBuffer(SIO)
+    console.log('SESSION', SESSION);
+    SIO = new S3_IO();
+    IB = new ImageBuffer(SIO);
 
-    CheckPointer = new MechanicalTurkCheckPointer(GAME_PACKAGE)
-    await CheckPointer.build()
-    TaskStreamer = new TaskStreamerClass(GAME_PACKAGE, IB, CheckPointer)
-    var trial_buffers = []
-    for (var i = 0; i < TASK_SEQUENCE.length; i++){
+    CheckPointer = new MechanicalTurkCheckPointer(GAME_PACKAGE);
+    await CheckPointer.build();
+    TaskStreamer = new TaskStreamerClass(GAME_PACKAGE, IB, CheckPointer);
+    var trial_buffers = [];
+    for (var i = 0; i < TASK_SEQUENCE.length; i++) {
         trial_buffers.push(TaskStreamer.build(i, TASK_SEQUENCE[i]['minTrialsCriterion']))
     }
     await Promise.all(trial_buffers);
-    DataWriter = new MechanicalTurkDataWriter(SESSION['assignmentId'], SESSION['hitId'], SESSION['inSandboxMode'])
+    DataWriter = new MechanicalTurkDataWriter(SESSION['assignmentId'], SESSION['hitId'], SESSION['inSandboxMode']);
 
     var playspacePackage = {
         'playspace_degreesVisualAngle': ENVIRONMENT['playspace_degreesVisualAngle'],
@@ -46,18 +46,17 @@ async function setup_mechanicalturk_session(sessionPackage) {
         'periodicRewardIntervalMsec': GAME['periodicRewardIntervalMsec'],
         'periodicRewardAmount': GAME['periodicRewardAmount'],
         'bonusUSDPerCorrect': ENVIRONMENT['bonusUSDPerCorrect'],
-        'juiceRewardPer1000Trials': ENVIRONMENT['juiceRewardPer1000Trials']
-    }
-    Playspace = new PlaySpaceClass(playspacePackage)
-    await Playspace.build()
+    };
+    Playspace = new PlaySpaceClass(playspacePackage);
+    await Playspace.build();
 
-    console.log(ENVIRONMENT)
-    UX = new MechanicalTurkUX(GAME['minimumTrials'], GAME['maximumTrials'], ENVIRONMENT['bonusUSDPerCorrect'])
+    console.log(ENVIRONMENT);
+    UX = new MechanicalTurkUX(GAME['minimumTrials'], GAME['maximumTrials'], ENVIRONMENT['bonusUSDPerCorrect']);
 
     // Convenience - if debugging on my machine, skip instructions etc.
     if (window.location.href.indexOf('localhost') !== -1) {
-        var show_instructions = true
-        var show_hand_selection = true
+        var show_instructions = true;
+        var show_hand_selection = true;
         if (ENVIRONMENT['showDeviceSelectionSplash'] != undefined) {
             var show_device_selection = ENVIRONMENT['showDeviceSelectionSplash']
         } else {
@@ -66,37 +65,37 @@ async function setup_mechanicalturk_session(sessionPackage) {
 
         var run_preview_mode = false
     } else {
-        var show_instructions = true
-        var show_hand_selection = true
-        if (ENVIRONMENT['showDeviceSelectionSplash'] != undefined) {
+        var show_instructions = true;
+        var show_hand_selection = true;
+        if (ENVIRONMENT['showDeviceSelectionSplash'] !== undefined) {
             var show_device_selection = ENVIRONMENT['showDeviceSelectionSplash']
         } else {
             var show_device_selection = true
         }
 
 
-        if (SESSION['assignmentId'] == 'assignmentId_not_found' || SESSION['assignmentId'] == 'ASSIGNMENT_ID_NOT_AVAILABLE') {
+        if (SESSION['assignmentId'] === 'assignmentId_not_found' || SESSION['assignmentId'] === 'ASSIGNMENT_ID_NOT_AVAILABLE') {
             var run_preview_mode = true
         } else {
             var run_preview_mode = false
         }
     }
 
-    if (run_preview_mode == true) {
-        console.log('RUNNING IN PREVIEW MODE')
-        var tutorialImage = await SIO.load_image('tutorial_images/TutorialClickMe.png')
-        UX.show_preview_splash()
+    if (run_preview_mode === true) {
+        console.log('RUNNING IN PREVIEW MODE');
+        var tutorialImage = await SIO.load_image('tutorial_images/TutorialClickMe.png');
+        UX.show_preview_splash();
         while (true) {
             await Playspace.run_tutorial_trial(tutorialImage)
         }
     }
 
-    if (show_instructions == true) {
+    if (show_instructions === true) {
         await UX.run_instructions_dialogue(ENVIRONMENT['instructionsDialogueString'])
     }
 
 
-    if (show_hand_selection == true) {
+    if (show_hand_selection === true) {
         SESSION['handedness'] = await UX.run_hand_selection_dialogue()
     }
 
@@ -105,17 +104,17 @@ async function setup_mechanicalturk_session(sessionPackage) {
         SESSION['inputDevice'] = await UX.run_device_selection_dialogue()
     }
 
-    TaskStreamer.debug2record()
-    Playspace.debug2record()
-    DataWriter.debug2record()
-    UX.debug2record()
+    TaskStreamer.debug2record();
+    Playspace.debug2record();
+    DataWriter.debug2record();
+    UX.debug2record();
 
-    var freturn = {}
-    freturn['TaskStreamer'] = TaskStreamer
-    freturn['DataWriter'] = DataWriter
-    freturn['Playspace'] = Playspace
-    freturn['UX'] = UX
-    freturn['SESSION'] = SESSION
+    var freturn = {};
+    freturn['TaskStreamer'] = TaskStreamer;
+    freturn['DataWriter'] = DataWriter;
+    freturn['Playspace'] = Playspace;
+    freturn['UX'] = UX;
+    freturn['SESSION'] = SESSION;
     return freturn
 }
 
